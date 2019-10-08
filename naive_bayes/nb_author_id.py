6deb8a792f1ca01fdca76d27836ae0b08b32d4b7
priyapande <priyapande07@gmail.com>
2019-10-09 00:43:58 +0530

    made tool compatible with python 3 and tested classfier svm and nb

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -12,6 +12,7 @@
     
 import sys
 from time import time
+
 sys.path.append("../tools/")
 from email_preprocess import preprocess
 
@@ -21,13 +22,20 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
-
-
 #########################################################
 ### your code goes here ###
+from sklearn.naive_bayes import GaussianNB
+t0 = time()
+clf = GaussianNB()
+clf.fit(features_train, labels_train)
+print("training time:", round(time()-t0, 3), "s")
+t1 = time()
+pred = clf.predict(features_test)
+print("prediction time:", round(time()-t1, 3), "s")
 
-
+from sklearn.metrics import accuracy_score
+acc_score = accuracy_score(labels_test, pred)
+print("Accuracy:", acc_score)
 #########################################################
 
 
